db1990a4fa0cfe18e21d21b4555b4deaad3aadfa
Ed Bassett <ed@devsketchpad.com>
2026-07-27 09:06:26 +1000

    swoosh 0.1.11 (aarch64)

--- Formula/swoosh.rb
+++ Formula/swoosh.rb
@@ -33,8 +33,8 @@ class Swoosh < Formula
   on_macos do
     on_arm do
       # TODO: replace url/sha256 with the real aarch64 tarball from release.sh output.
-      url "https://github.com/bitsurgery/swoosh/releases/download/v0.1.10/swoosh-0.1.10-aarch64-apple-darwin.tar.gz"
-      sha256 "e1d2329af094a48726d55bf3d5178fefe2b97dfbc9e852ed2922677d7ecc6075"
+      url "https://github.com/bitsurgery/swoosh/releases/download/v0.1.11/swoosh-0.1.11-aarch64-apple-darwin.tar.gz"
+      sha256 "1fdf1950e87f5679d1900d88c94a7590f7cc41e3176749fdaa98c1dab01019bd"
     end
 
     on_intel do
